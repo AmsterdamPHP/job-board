49841699abbcaddd2a45075027c1d680fe2ceeac
Pascal de Vink <pascal.de.vink@gmail.com>
2013-04-16 22:01:31 +0200

    #13: Added archiving of jobs

--- src/AmsterdamPHP/AdminBundle/Controller/JobController.php
+++ src/AmsterdamPHP/AdminBundle/Controller/JobController.php
@@ -3,6 +3,9 @@
 namespace AmsterdamPHP\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityRepository;
+use AmsterdamPHP\JobBundle\Entity\Job;
+use Doctrine\ORM\EntityManager;
 
 class JobController extends Controller
 {
@@ -18,5 +21,24 @@ class JobController extends Controller
         return $this->render('AmsterdamPHPAdminBundle:Job:index.html.twig', array(
             'entities' => $entities,
         ));
+    }
+
+    public function archiveAction($id)
+    {
+        /** @var $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var $repository EntityRepository */
+        $repository = $em->getRepository('AmsterdamPHPJobBundle:Job');
+
+        $criteria = array('id' => $id);
+        /** @var $entity Job */
+        $entity = $repository->findOneBy($criteria);
+
+        $entity->setArchived(true);
+        $em->persist($entity);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('admin_jobs'));
     }
 }
